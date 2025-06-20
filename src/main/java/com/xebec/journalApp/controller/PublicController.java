429b835686ac3e3d7ced409af2ad0beb70178d53
Mohd Talha <talhamoh017@gmail.com>
2025-06-21 03:52:32 +0530

    Adding Spring Security

---
+++ src/main/java/com/xebec/journalApp/controller/PublicController.java
@@ -0,0 +1,26 @@
+package com.xebec.journalApp.controller;
+
+import com.xebec.journalApp.entity.User;
+import com.xebec.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "OK";
+    }
+
+    @PostMapping("/create-user")
+    public void createUser(@RequestBody User user){
+        userService.saveEntry(user);
+    }
+}
