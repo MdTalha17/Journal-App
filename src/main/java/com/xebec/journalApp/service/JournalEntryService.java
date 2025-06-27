9f1e9f2ea261d2b8dfb1ead0b0db4ea917c7f03e
Mohd Talha <talhamoh017@gmail.com>
2025-06-27 18:45:48 +0530

    Logging Files

--- src/main/java/com/xebec/journalApp/service/JournalEntryService.java
+++ src/main/java/com/xebec/journalApp/service/JournalEntryService.java
@@ -4,6 +4,8 @@ import com.xebec.journalApp.entity.JournalEntry;
 import com.xebec.journalApp.entity.User;
 import com.xebec.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
--- src/main/java/com/xebec/journalApp/service/UserService.java
+++ src/main/java/com/xebec/journalApp/service/UserService.java
@@ -2,7 +2,10 @@ package com.xebec.journalApp.service;
 
 import com.xebec.journalApp.entity.User;
 import com.xebec.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -27,6 +31,11 @@ public class UserService {
             userRepository.save(user);
             return true;
         } catch (Exception e) {
+            log.error("Error Occurred for {} :", user.getUserName(), e);
+            log.warn("hehehehehehe");
+            log.info("hehehehehehe");
+            log.debug("hehehehehehe");
+            log.trace("hehehehehehe");
             return false;
         }
     }
